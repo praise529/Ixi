bb22bbcf467a286954164b30a515e94da36a15fd
Praise Olatope <aanuolatope@gmail.com>
2026-03-28 15:26:55 +0000

    bye

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
+import { Tasks } from "../Data/Tasks";
 import "./App.css";
 import Task from "./Components/Task";
 import TopBar from "./Components/TopBar";
 
 export default function App() {
-    return (
-        <div>
-            <TopBar />
-            <button>+ Add Task</button>
+  return (
+    <div>
+      <TopBar />
+      <button>+ Add Task</button>
 
-            <div className="Main">
-                <div class="Tasks">
-                    <Task content="Task 1" />
-                    <Task content="Task 1" />
-                    <Task content="Task 1" />
-                    <Task content="Task 1" />
-                    <Task content="Task 1" />
-                </div>
-            </div>
-        </div>
-    )
+      <div className="Main">
+        <div class="Tasks">{Tasks.length > 0 ? <></> : <div></div>}</div>
+      </div>
+    </div>
+  );
 }
